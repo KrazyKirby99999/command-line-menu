be544ed80349c9f2636f0d68847c13ce4c808c5a
KrazyKirby99999 <isc.bev@gmail.com>
2020-10-22 19:32:02 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,11 +1,22 @@
 from setuptools import setup
 
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
 setup(name='Command Line Menu',
       version='0.2.2',
       description='A simple module to create a menu for command line programs.',
+      long_description=long_description,
+      long_description_content_type="text/markdown",
       url='https://github.com/KrazyKirby99999/command-line-menu',
       author='KrazyKirby99999',
       author_email='krazykirby99999@gmail.com',
       license='GNU AGPLv3',
-      packages=['command_line_menu'],
-      zip_safe=False)
+      packages=setuptools.find_packages(),
+      zip_safe=False,
+      classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU AGPLv3 License",
+        "Operating System :: OS Independent",
+    ],
+      )
